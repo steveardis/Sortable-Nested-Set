59762455a31ffd6acc6b58b5cf933dcbaff534a9
Zykin Ilya <zykin-ilya@ya.ru>
2011-05-02 21:10:15 +0400

    init

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'mysql2'
+gem 'nested_set'
+gem 'haml'
+gem 'jquery-rails'
+gem 'faker'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SortableNestedSet::Application.config.secret_token = 'd84c3e08e068e0d5da821aefcbe90dbe9160edf8617d4e47be039e21f5c330dd2934c8bd63c31fe40fe04d27104e918408e3efaec97a9338858c1d246a7d5ba1'
